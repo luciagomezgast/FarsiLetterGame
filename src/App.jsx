e49b582e7296bbbc2bd8311db8a877021c862156
gonsolvis <121898200+gonsolvis@users.noreply.github.com>
2023-05-24 12:02:39 +0200

    bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,71 @@
 import "./App.css";
-import data from "./data";
+import cards from "./data";
 import "bootstrap/dist/css/bootstrap.css";
 import LetterComp from "./LetterComp";
 import SymbolComp from "./SymbolComp";
 import { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 
+const AMOUNT_OF_CARDS = 3
+
 function App() {
-  const [cards, setCards] = useState(data);
-  const [randomCards, setRandomCards] = useState(getThreeRandomSymbolCards());
-  const [isMatch, setIsMatch] = useState(false);
+  const [randomCards, setRandomCards] = useState([]);
   const [score, setScore] = useState(0);
+  const [randomIndex, setRandomIndex] = useState(0)
 
 
-  function getThreeRandomSymbolCards() {
-    let newArrayOfSymbolCards = [];
-    let amountOfCardsWanted = 3
+  function setRandomSymbolCards() {
+    const newArrayOfSymbolCards = [];
+    
+    while (newArrayOfSymbolCards.length < AMOUNT_OF_CARDS) {
+      const randomIndexNumberOfDataArr = Math.floor(Math.random() * cards.length);
 
-    while (newArrayOfSymbolCards.length < amountOfCardsWanted) {
-      let randomIndexNumber = Math.floor(Math.random() * cards.length);
-
-      if (!newArrayOfSymbolCards.includes(randomIndexNumber)) {
-        newArrayOfSymbolCards.push(randomIndexNumber);
+      if (!newArrayOfSymbolCards.includes(randomIndexNumberOfDataArr)) {
+        newArrayOfSymbolCards.push(randomIndexNumberOfDataArr);
       }
     }
 
-    return newArrayOfSymbolCards.map((index) => cards[index]);
+    setRandomCards(newArrayOfSymbolCards.map((index) => cards[index]));
+    setRandomIndex(Math.floor(Math.random() * AMOUNT_OF_CARDS))
   }
 
-
-  const randomIndexOutOfThree = Math.floor(Math.random() * 3)
-
-
+  //clickhandler
   function ChecksIfMatchOnClick(id) {
-    let savedLetterCompId = randomCards[randomIndexOutOfThree].id
+    let savedLetterCompId = randomCards[randomIndex].id
 
-    if (id == savedLetterCompId) {
+    if (id === savedLetterCompId) {
  
       setScore((prevScore) => prevScore + 1); // Increase the score by 1
-      setIsMatch(true)
+      setRandomSymbolCards();
       console.log("we have a match")
 
      
     } else {
-      // setScore((prevScore) => prevScore - 1); // Decrease the score by 1
+   
+      setScore((prevScore) => prevScore - 1); // Decrease the score by 1
       console.log("we DO NOT have a match TRY AGAIN");
     
     }
   }
 
-
-
   useEffect(() => {
-    setRandomCards(getThreeRandomSymbolCards());
-    setIsMatch(false)
-    
-  
-  }, [isMatch]);
+    setRandomSymbolCards();
+  }, [])
 
 
 
-  return (
+  return randomCards.length ? (
     <div className="outsideApp">
       <NavBar 
       currentScore ={score}
       />
 
       <div className="App">
+
         <div className="letterBox">
           <LetterComp
-            id={randomCards[randomIndexOutOfThree].id}
-            letter={randomCards[randomIndexOutOfThree].Letter}
+            id={randomCards[randomIndex].id}
+            letter={randomCards[randomIndex].Letter}
           />
         </div>
 
@@ -86,7 +81,7 @@ function App() {
         </div>
       </div>
     </div>
-  );
+  ) : null;
 }
 
 export default App;
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,30 +1,43 @@
-
-import './App.css'
+import "./NavBar.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 function NavBar(props) {
   return (
-    <nav id="navid" className="navbar navbar-expand  " >
-      <a id="Navbar" className="navbar-brand" href="#">Farsi Game </a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <nav id="navid" className="navbar navbar-expand  ">
+      <a id="Navbar" className="navbar-brand" href="#">
+        Farsi Game{" "}
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a id="home" className="nav-link " href="#">Home </a>
+            <a id="home" className="nav-link " href="#">
+              Home
+            </a>
           </li>
         </ul>
-        <ul>
-            <li className="nav-item active">
-              <a id="score" className="nav-link "> Current Score : {props.currentScore} </a>
-            </li>
-          </ul>
-          </div>  
-  
-
+      </div>
+      <div className="score">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a id="score" className="nav-link ">
+              Current Score : {props.currentScore}
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
 
-export default NavBar
+export default NavBar;
