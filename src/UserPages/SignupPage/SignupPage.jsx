5173dc74895739eb463ef3189e43e200d55d2605
gonsolvis <121898200+gonsolvis@users.noreply.github.com>
2023-06-11 14:25:29 +0200

    will fix deploy error

--- src/UserPages/SignupPage/SignupPage.jsx
+++ src/UserPages/SignupPage/SignupPage.jsx
@@ -1,42 +1,11 @@
-/*  eslint-disable*/
 import "../LoginPage/loginPage.css";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-// import authService from "../../services/auth.service";
+import { Link } from "react-router-dom";
 
 function SignupPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const [errorMessage, setErrorMessage] = useState(undefined);
-
-  // const navigate = useNavigate();
-
-  // const handleEmail = (e) => setEmail(e.target.value);
-  // const handlePassword = (e) => setPassword(e.target.value);
-  // const handleName = (e) => setName(e.target.value);
-
-  // const handleSignupSubmit = (e) => {
-  //   e.preventDefault();
-  //   const requestBody = { email, password, name };
-  //   // authService
-  //     .signup(requestBody)
-  //     .then((response) => {
-  //       console.log("do i get here", response);
-  //       navigate("/login");
-  //     })
-  //     .catch((error) => {
-  //       const errorDescription = error.response.data.message;
-  //       setErrorMessage(errorDescription);
-  //     });
-  // };
-
   return (
     <div className="LoginPage">
       <div className="LoginPage-box">
         <h1>Sign Up</h1>
-
-        {/* <form onSubmit={handleSignupSubmit} className="login--form  "> */}
 
         <form className="login--form  ">
           <label>Name:</label>
@@ -44,7 +13,6 @@ function SignupPage() {
             type="text"
             name="name"
             value={name}
-            // onChange={handleName}
             className="login--input"
           />
 
@@ -53,7 +21,6 @@ function SignupPage() {
             type="email"
             name="email"
             value={email}
-            // onChange={handleEmail}
             className="login--input"
           />
 
@@ -62,20 +29,17 @@ function SignupPage() {
             type="password"
             name="password"
             value={password}
-            // onChange={handlePassword}
             className="login--input"
           />
 
           <button type="submit" className="login--loginButton">
             {" "}
-          Register
+            Register
           </button>
         </form>
 
-        {/* {errorMessage && <p className="error-message">{errorMessage}</p>} */}
-
         <p className="login--p">
-          Already have account?   {" "}
+          Already have account?{" "}
           <Link to={"/login"} className="login--signupLink">
             Login
           </Link>
